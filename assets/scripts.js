24d846a622364ccbe01b155cba4c4b9a45b88b7c
viniciusmouraesilva <viniciusmouraesilva2204@outlook.com>
2018-06-10 15:09:26 -0300

    Adicionano Script Js

---
+++ assets/scripts.js
@@ -0,0 +1,9 @@
+var banner = ["Os melhores do Brasil", "Qualidade e preço baixo!"]
+var bannerAtual = 0;
+
+function trocarBanner() {
+	bannerAtual = (bannerAtual + 1) % 2
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
